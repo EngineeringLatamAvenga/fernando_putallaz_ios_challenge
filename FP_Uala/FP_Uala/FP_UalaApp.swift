98f3a109294ab5fe60c814c1000dee3eaa66ffee
Fernando Putallaz <ferhedio@gmail.com>
2025-01-02 13:12:40 -0300

    Initial commit - Project created

---
+++ FP_Uala/FP_Uala/FP_UalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FP_UalaApp.swift
+//  FP_Uala
+//
+//  Created by Fernando Putallaz on 02/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FP_UalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
